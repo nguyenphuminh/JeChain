89e529f24a04bdeaf257b5cc9ee0d0b04dd53b88
Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2021-10-28 16:45:10 +0700

    Update jechain.js

--- jechain.js
+++ jechain.js
@@ -23,7 +23,7 @@ class Block {
 
 class Blockchain {
     constructor() {
-        this.chain = [new Block(Date.now().toString(), {}, "")];
+        this.chain = [new Block(Date.now().toString())];
         this.difficulty = 1;
     }
 
