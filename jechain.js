232861ebb5dface3cb812b5048bb4b5a3f998661
Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2021-10-31 18:07:33 +0700

    Update jechain.js

--- jechain.js
+++ jechain.js
@@ -14,7 +14,7 @@ class Block {
     }
 
     mine(difficulty) {
-        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+        while(!this.hash.startsWith(Array(difficulty + 1).join("0"))) {
             this.nonce++;
             this.hash = this.getHash();
         }
