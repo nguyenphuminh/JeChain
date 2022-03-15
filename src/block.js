47c00383f696ecbdb94391f3b98d838de3ad1c19
Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2022-03-15 12:35:17 +0700

    Update block.js

--- src/block.js
+++ src/block.js
@@ -1,6 +1,5 @@
 const crypto = require("crypto"), SHA256 = message => crypto.createHash("sha256").update(message).digest("hex");
 const EC = require("elliptic").ec, ec = new EC("secp256k1");
-const { totalmem } = require("os");
 const Transaction = require("./transaction");
 const MINT_PRIVATE_ADDRESS = "0700a1ad28a20e5b2a517c00242d3e25a88d84bf54dce9e1733e6096e6d6495e";
 const MINT_KEY_PAIR = ec.keyFromPrivate(MINT_PRIVATE_ADDRESS, "hex");
