f9ab8a4f600470456693d30607e8a20dd611d3a5
Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2022-07-26 21:38:35 +0700

    Log out address from keygen.js

--- utils/keygen.js
+++ utils/keygen.js
@@ -1,6 +1,13 @@
+const crypto = require("crypto"), SHA256 = message => crypto.createHash("sha256").update(message).digest("hex");
+
 const EC = require("elliptic").ec, ec = new EC("secp256k1");
 
 const keyPair = ec.genKeyPair();
 
-console.log("Public key:", keyPair.getPublic("hex"));
-console.log("Private key:", keyPair.getPrivate("hex"));
+const pubKey = keyPair.getPublic("hex");
+const privKey = keyPair.getPrivate("hex");
+const address = SHA256(pubKey);
+
+console.log("Address:", address);
+console.log("Private key:", privKey);
+console.log("Public key:", pubKey);
