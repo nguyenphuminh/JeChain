a75a59967f17fa986f163d398098281662ce2451
Simone Gosetto <simone.gosetto@gmail.com>
2022-11-21 09:49:11 +0100

    feat: unification of mint account parameters in "global.share"

--- src/core/genesis.js
+++ src/core/genesis.js
@@ -5,9 +5,7 @@ const Block = require("./block");
 const Transaction = require("./transaction");
 const { INITIAL_SUPPLY } = require("../config.json");
 
-const MINT_PRIVATE_ADDRESS = "0700a1ad28a20e5b2a517c00242d3e25a88d84bf54dce9e1733e6096e6d6495e";
-const MINT_KEY_PAIR = ec.keyFromPrivate(MINT_PRIVATE_ADDRESS, "hex");
-const MINT_PUBLIC_ADDRESS = MINT_KEY_PAIR.getPublic("hex");
+const {MINT_PRIVATE_ADDRESS, MINT_KEY_PAIR, MINT_PUBLIC_ADDRESS} = global.share;
 
 function generateGenesisBlock() {
     const firstMint = new Transaction(
--- src/node/server.js
+++ src/node/server.js
@@ -20,9 +20,7 @@ const { parseJSON } = require("../utils/utils");
 const jelscript = require("../core/runtime");
 const { buildMerkleTree } = require("../core/merkle");
 
-const MINT_PRIVATE_ADDRESS = "0700a1ad28a20e5b2a517c00242d3e25a88d84bf54dce9e1733e6096e6d6495e";
-const MINT_KEY_PAIR = ec.keyFromPrivate(MINT_PRIVATE_ADDRESS, "hex");
-const MINT_PUBLIC_ADDRESS = MINT_KEY_PAIR.getPublic("hex");
+const {MINT_PRIVATE_ADDRESS, MINT_KEY_PAIR, MINT_PUBLIC_ADDRESS} = global.share;
 
 const opened    = [];  // Addresses and sockets from connected nodes.
 const connected = [];  // Addresses from connected nodes.
